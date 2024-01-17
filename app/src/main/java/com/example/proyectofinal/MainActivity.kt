e6f0ca8f3d4ffb6a9eee8a878f957a41a463e196
Daniel Szaŀkowski <10836979@ieselgrao.org>
2024-01-17 12:17:43 +0100

    Scaffold empezado

--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -1,16 +1,59 @@
 package com.example.proyectofinal
 
+import android.graphics.ColorFilter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.proyectofinal.ui.theme.ProyectoFinalTheme
+import kotlinx.coroutines.launch
+import java.security.Principal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +62,7 @@ class MainActivity : ComponentActivity() {
             ProyectoFinalTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -27,17 +70,100 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
+fun Principal() {
+    FuncionScaffold()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopAppBar() {
+    TopAppBar(
+        title = { Text("Top App Bar")},
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.DarkGray
+        , titleContentColor = Color.White),
+        navigationIcon = {
+            IconButton(onClick = { }) { Icon(Icons.Filled.Menu, contentDescription = "Desc", tint = Color.White) }
+        },
+        actions = {
+            IconButton(onClick = {}) { Icon(Icons.Filled.Add, contentDescription = "Desc", tint = Color.White) }
+            Spacer(modifier = Modifier.size(6.dp))
+            IconButton(onClick = {}) { Icon(Icons.Filled.Close, contentDescription = "Desc", tint = Color.White) }
+        },
     )
+}
+
+@Composable
+fun MyBottomNavigation() {
+    var index by rememberSaveable { mutableIntStateOf(0) }
+    NavigationBar(
+        containerColor = Color.DarkGray,
+        contentColor = Color.White
+    ) {
+        NavigationBarItem(
+            selected = index == 0,
+            onClick = { index = 0 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = "Home Icon"
+                )
+            },
+            label = { Text(text ="Home",
+                color = Color.White)}
+        )
+        NavigationBarItem(
+            selected = index == 2,
+            onClick = { index = 2 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Person Icon",
+                    tint = Color.White
+                )
+            },
+            label = { Text(text = "Person",
+                color = Color.White) }
+        )
+    }
+}
+
+@Composable
+fun FuncionScaffold() {
+    Scaffold(
+        topBar = { MyTopAppBar() },
+        content = { innerPadding ->
+            MyContent(innerPadding)
+        },
+        bottomBar = { MyBottomNavigation() },
+    )
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun MyContent(innerPadding: PaddingValues) {
+    val colors = listOf(Color(0xFFffd7d7.toInt()),
+        Color(0xFFffe9d6.toInt()),
+        Color(0xFFfffbd0.toInt()),
+        Color(0xFFe3ffd9.toInt()),
+        Color(0xFFd0fff8.toInt()))
+    LazyColumn(
+        modifier = Modifier.consumeWindowInsets(innerPadding),
+        contentPadding = innerPadding
+    ) {
+        items(100) { count ->
+            Box(Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .background(colors[count % colors.size])
+            ) { Text(text = "Fila $count") }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ProyectoFinalTheme {
-        Greeting("Android")
+        Principal()
     }
 }
