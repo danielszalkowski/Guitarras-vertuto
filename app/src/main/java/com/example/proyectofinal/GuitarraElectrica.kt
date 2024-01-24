c7ea9b7c77ea57abc11c3679818d65fe387b441a
Daniel Szaŀkowski <10836979@ieselgrao.org>
2024-01-24 12:13:23 +0100

    Guitarras principio

---
+++ app/src/main/java/com/example/proyectofinal/GuitarraElectrica.kt
@@ -0,0 +1,6 @@
+package com.example.proyectofinal
+
+import androidx.annotation.DrawableRes
+
+data class GuitarraElectrica(val modelo: String, val color: String, val nTrastes: Int, val precio: String, @DrawableRes var imagen: Int)
+
--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.proyectofinal
 
 import android.graphics.ColorFilter
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -15,7 +21,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
@@ -23,6 +34,10 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -45,11 +60,16 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.proyectofinal.ui.theme.ProyectoFinalTheme
 import kotlinx.coroutines.launch
@@ -62,7 +82,7 @@ class MainActivity : ComponentActivity() {
             ProyectoFinalTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-
+                    Principal()
                 }
             }
         }
@@ -78,16 +98,16 @@ fun Principal() {
 @Composable
 fun MyTopAppBar() {
     TopAppBar(
-        title = { Text("Top App Bar")},
-        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.DarkGray
-        , titleContentColor = Color.White),
+        title = { Text("Guitarras Eléctricas")},
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.LightGray
+        , titleContentColor = Color.Black),
         navigationIcon = {
-            IconButton(onClick = { }) { Icon(Icons.Filled.Menu, contentDescription = "Desc", tint = Color.White) }
+            IconButton(onClick = { }) { Icon(Icons.Filled.Menu, contentDescription = "Desc", tint = Color.Black) }
         },
         actions = {
-            IconButton(onClick = {}) { Icon(Icons.Filled.Add, contentDescription = "Desc", tint = Color.White) }
+            IconButton(onClick = {}) { Icon(Icons.Filled.Add, contentDescription = "Desc", tint = Color.Black) }
             Spacer(modifier = Modifier.size(6.dp))
-            IconButton(onClick = {}) { Icon(Icons.Filled.Close, contentDescription = "Desc", tint = Color.White) }
+            IconButton(onClick = {}) { Icon(Icons.Filled.Close, contentDescription = "Desc", tint = Color.Black) }
         },
     )
 }
@@ -96,33 +116,35 @@ fun MyTopAppBar() {
 fun MyBottomNavigation() {
     var index by rememberSaveable { mutableIntStateOf(0) }
     NavigationBar(
-        containerColor = Color.DarkGray,
-        contentColor = Color.White
+        containerColor = Color.LightGray,
+        contentColor = Color.Black
     ) {
         NavigationBarItem(
             selected = index == 0,
-            onClick = { index = 0 },
+            onClick = { index = 0  },
             icon = {
                 Icon(
                     imageVector = Icons.Default.Home,
-                    contentDescription = "Home Icon"
+                    contentDescription = "Home Icon",
+                    tint = Color.Black
                 )
             },
             label = { Text(text ="Home",
-                color = Color.White)}
+                color = Color.Black)}
         )
         NavigationBarItem(
-            selected = index == 2,
-            onClick = { index = 2 },
+            selected = index == 1,
+            onClick = { index = 1 },
             icon = {
                 Icon(
                     imageVector = Icons.Default.Person,
                     contentDescription = "Person Icon",
-                    tint = Color.White
+                    tint = Color.Black
                 )
             },
             label = { Text(text = "Person",
-                color = Color.White) }
+                color = Color.Black)
+            }
         )
     }
 }
@@ -132,12 +154,33 @@ fun FuncionScaffold() {
     Scaffold(
         topBar = { MyTopAppBar() },
         content = { innerPadding ->
-            MyContent(innerPadding)
+            GuitarGridView(innerPadding)
         },
         bottomBar = { MyBottomNavigation() },
     )
 }
 
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun GuitarGridView(innerPadding: PaddingValues) {
+    val context = LocalContext.current
+    LazyVerticalGrid(
+        modifier = Modifier
+            .consumeWindowInsets(innerPadding)
+            .padding(8.dp),
+        columns = GridCells.Fixed(1),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = innerPadding,
+        content = {
+            items(getGuitars()) {
+                ItemHero(it) {
+                    Toast.makeText(context, it.modelo, Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+}
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun MyContent(innerPadding: PaddingValues) {
@@ -151,13 +194,57 @@ fun MyContent(innerPadding: PaddingValues) {
         contentPadding = innerPadding
     ) {
         items(100) { count ->
-            Box(Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-                .background(colors[count % colors.size])
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .background(colors[count % colors.size])
             ) { Text(text = "Fila $count") }
         }
     }
+}
+
+@Composable
+fun ItemHero(guitarra: GuitarraElectrica, onItemSelected: (GuitarraElectrica) -> Unit) {
+    Card(border = BorderStroke(2.dp, Color.Black),
+        shape = RoundedCornerShape(4.dp),
+        modifier = Modifier
+            .width(250.dp)
+            .clickable { onItemSelected(guitarra) }) {
+        Column() {
+            Image(
+                painter = painterResource(id = guitarra.imagen),
+                contentDescription = "SuperHero Avatar",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = guitarra.modelo,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = guitarra.color,
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                fontSize = 12.sp
+            )
+            Text(
+                text = guitarra.precio,
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(6.dp),
+                fontSize = 10.sp
+            )
+        }
+    }
+}
+
+fun getGuitars(): List<GuitarraElectrica> {
+    return listOf(
+        GuitarraElectrica("ESP LTD EX-400", "Blanco", 22, "1000€", R.drawable.ltdex400),
+        GuitarraElectrica("Harley Benton CST-24", "Negro", 24, "250€",R.drawable.hbentoncst24),
+        GuitarraElectrica("PRS SE Mikael Akerfeldt", "Madera", 24, "800€", R.drawable.opeth),
+        GuitarraElectrica("ESP LTD KH-602", "Morado brillante", 24, "1000€", R.drawable.ltdkh602)
+    )
 }
 
 @Preview(showBackground = true)
